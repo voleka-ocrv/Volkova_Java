c174301372ecca16d6252febe28c7ce0d57ba35a
voleka-ocrv <48094534+voleka-ocrv@users.noreply.github.com>
2023-02-14 12:31:28 +0300

    updade import

--- Task_3/src/com/volkova/core/Main.java
+++ Task_3/src/com/volkova/core/Main.java
@@ -3,14 +3,8 @@ package com.volkova.core;
 import com.volkova.core.utils.ArrayChecker;
 import com.volkova.core.utils.ConsoleReader;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class Main {
 
